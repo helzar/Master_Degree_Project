365203ac6433e4deb82f8216cc52e6263a83a1d6
Volodymyr_Roman <volodyaroman9@gmail.com>
2016-11-23 23:03:13 +0200

    Added Spring Boot Application entry point

---
+++ src/main/java/com/vovan/dwh/MasterDegreeDwhProjectApplication.java
@@ -0,0 +1,12 @@
+package com.vovan.dwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterDegreeDwhProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterDegreeDwhProjectApplication.class, args);
+	}
+}
